277ca0d05d1a1e57b8eef3296866bb3e855cb8f3
Roman.Savchenko <Roman.Savchenko@armadalabs.com>
2018-07-11 18:05:16 +0300

    added swagger and db

--- server/src/main.hmr.ts
+++ server/src/main.hmr.ts
@@ -1,15 +1,30 @@
 import { NestFactory, FastifyAdapter } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { getConnection } from '../node_modules/typeorm';
 
 declare const module: any;
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, new FastifyAdapter());
+    const app = await NestFactory.create(AppModule, new FastifyAdapter()),
+        options = new DocumentBuilder()
+            .setTitle('Market api')
+            .setDescription('The market API description')
+            .setVersion('1.0')
+            .addTag('market')
+            .build(),
+        document = SwaggerModule.createDocument(app, options);
+
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(3000);
 
     if (module.hot) {
         module.hot.accept();
-        module.hot.dispose(() => app.close());
+        module.hot.dispose(async () => {
+            await getConnection().close();
+            await app.close();
+        });
     }
 }
 bootstrap();
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory, FastifyAdapter } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '../node_modules/@nestjs/swagger';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, new FastifyAdapter());
+    const app = await NestFactory.create(AppModule, new FastifyAdapter()),
+        options = new DocumentBuilder()
+            .setTitle('Market api')
+            .setDescription('The market API description')
+            .setVersion('1.0')
+            .addTag('market')
+            .build(),
+        document = SwaggerModule.createDocument(app, options);
+
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(3000);
 }
 bootstrap();
