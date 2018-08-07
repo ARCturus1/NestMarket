fa516620637bcf0672379d8afd80ef8f63af30a0
ARCturus1 <arcturus1@yandex.ru>
2018-08-08 01:35:05 +0300

    some changes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { AppMaterialModule } from './material/material.module';
+import { RootMenuComponent } from './views/root-menu/root-menu.component';
+import { HelloMessageComponent } from './views/hello-message/hello-message.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        RootMenuComponent,
+        HelloMessageComponent
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
+        AppMaterialModule,
         ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
     ],
     providers: [],
---
+++ client/src/app/views/hello-message/hello-message.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-hello-message',
+    templateUrl: './hello-message.component.html',
+    styleUrls: ['./hello-message.component.css']
+})
+export class HelloMessageComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
